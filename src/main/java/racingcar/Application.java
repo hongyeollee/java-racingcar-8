af18d042bf9a899b9c34b74398604cabc780005f
이홍열 <zeler1004@gmail.com>
2025-10-25 02:17:30 +0900

    feat(application): add main function for car race process

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,11 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String inputCarName = InputView.carName();
+        int driveCount = InputView.driveCount();
+        Race race = new Race(inputCarName, driveCount,new MakeRandomNumber());
+        OutputView.printExecutionResult();
+        race.playAndPrintEachRound();
+        OutputView.printWinners(race.getWinners());
     }
 }
