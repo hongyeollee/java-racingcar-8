cfc96c7ec0a3b343d5bfba352e6489416198530c
이홍열 <zeler1004@gmail.com>
2025-10-25 02:14:01 +0900

    feat(OutputView): add OutputView class

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class OutputView {
+
+    public static void printExecutionResult() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public static void printRound(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        StringJoiner joiner = new StringJoiner(", ");
+        for (String winner : winners) {
+            joiner.add(winner);
+        }
+        System.out.println("최종 우승자 : " + joiner);
+    }
+}
